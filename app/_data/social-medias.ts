f1acb769786309db6c4452c818fe5cf0b77b49d9
sdh20282 <sdh20282@gmail.com>
2025-03-10 07:26:45 +0900

    chore: 소셜 인스타그램 삭제

--- app/_data/social-medias.ts
+++ app/_data/social-medias.ts
@@ -4,10 +4,6 @@ export const socialMedias = [
     title: 'GitHub',
   },
   {
-    href: 'https://www.instagram.com/sdh20282/',
-    title: 'Instagram',
-  },
-  {
     href: 'https://velog.io/@sdh20282',
     title: 'blog',
   }
